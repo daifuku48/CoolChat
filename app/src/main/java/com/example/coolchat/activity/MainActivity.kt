3fed5a6a7655c07055c01d193b9150423a723939
danil <natalya.haritonov.72@gmail.com>
2023-02-09 22:04:30 +0300

    sign up activity

--- app/src/main/java/com/example/coolchat/activity/MainActivity.kt
+++ app/src/main/java/com/example/coolchat/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.coolchat
+package com.example.coolchat.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,11 +6,13 @@ import android.text.Editable
 import android.text.InputFilter
 import android.text.TextWatcher
 import android.view.View
-import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
+import com.example.coolchat.R
 import com.example.coolchat.databinding.ActivityMainBinding
 import com.example.coolchat.model.Message
 import com.example.coolchat.model.MessageCustomAdapter
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -21,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var adapter: MessageCustomAdapter
     lateinit var database: Firebase
     lateinit var messagesDatabaseReference: DatabaseReference
-
+    lateinit var messageChildEventListener: ChildEventListener
     val username = "user"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +31,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val database = Firebase.database
         val messagesDatabaseReference = database.getReference("messages")
-        messagesDatabaseReference.setValue("aboba")
         binding.progressBarOfSending.visibility = View.INVISIBLE
         val listOfMessage = ArrayList<Message>()
         adapter = MessageCustomAdapter(this, R.layout.item_message, listOfMessage)
+        binding.messageListView.adapter = adapter
         binding.messageButton.isEnabled = false
         binding.textOfMessageEditText.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -42,11 +44,9 @@ class MainActivity : AppCompatActivity() {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 binding.messageButton.isEnabled = p0.toString().trim().isNotEmpty()
             }
-
             override fun afterTextChanged(p0: Editable?) {
 
             }
-
         })
 
         binding.textOfMessageEditText.filters = arrayOf<InputFilter>(
@@ -56,11 +56,40 @@ class MainActivity : AppCompatActivity() {
         )
 
         binding.messageButton.setOnClickListener {
+            val text = binding.textOfMessageEditText.text.toString()
+            val message = Message(text, username, 0)
+            messagesDatabaseReference.push().setValue(message)
             binding.textOfMessageEditText.text?.clear()
-            Toast.makeText(this, "button is active", Toast.LENGTH_LONG).show()
         }
+
         binding.sendPhotoButton.setOnClickListener{
 
         }
+
+
+        messageChildEventListener = object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val message = snapshot.getValue(Message::class.java)
+                adapter.add(message)
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        }
+
+        messagesDatabaseReference.addChildEventListener(messageChildEventListener)
     }
 }
