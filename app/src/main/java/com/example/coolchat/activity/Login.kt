3f6c320d3f3d3bf095eb7d6f5c0e234f71ab6a37
danil <natalya.haritonov.72@gmail.com>
2023-02-10 21:56:59 +0300

    sign up activity ui with fragments

--- app/src/main/java/com/example/coolchat/activity/Login.kt
+++ app/src/main/java/com/example/coolchat/activity/Login.kt
@@ -1,13 +1,54 @@
 package com.example.coolchat.activity
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.example.coolchat.R
+import com.example.coolchat.databinding.ActivityLoginBinding
+import com.google.android.material.tabs.TabLayout
 
 class Login : AppCompatActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager2: ViewPager2
+    lateinit var viewPagerAdapter: ViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager2 = findViewById(R.id.view_pager)
+        tabLayout.addTab(tabLayout.newTab().setText("SignUp")) //adding tabs to pager in login activity
+        tabLayout.addTab(tabLayout.newTab().setText("Login"))
+
+        val fragmentManager = supportFragmentManager
+        viewPagerAdapter = ViewPagerAdapter(fragmentManager, lifecycle)
+        viewPager2.adapter = viewPagerAdapter
+
+        tabLayout.setOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager2.currentItem = tab?.position!!
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+        })
+
+        viewPager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+                super.onPageSelected(position)
+            }
+        })
 
     }
 }
---
+++ app/src/main/java/com/example/coolchat/activity/ViewPagerAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.coolchat.activity
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return if (position == 1) {
+            LoginTabFragment()
+        } else SignUpTabFragment()
+    }
+
+}
